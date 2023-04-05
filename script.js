8cd704f0ce911d03be6c99f97cdc51a58be1488d
Marq W <marquisewest86@duck.com>
2023-04-04 23:04:52 -0500

    completed the overall structure of the program. Still need to account for the edge cases

--- script.js
+++ script.js
@@ -1,14 +1,72 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  //Building the character base...
+  let lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  let uppercaseLetter = lowercaseLetters.toUpperCase();
+  let numbers = "1234567890";
+  let specialChar = ";.<>!@#$%^&*()?/{}[]'";
+  let placeholder = '';
 
+  //Determine password length
+  let passLength = prompt("How many characters would you like your password to be? ");
+  
+  // Determine which characters will ultimately go into the character base...
+  let includeLowercaseLetters = prompt("Would you like to include lowercase letters? (yes or no)").toLowerCase();
+  if (includeLowercaseLetters === "yes") {
+    includeLowercaseLetters = true;
+  } else {
+    includeLowercaseLetters = false;
+  }
+
+  let includeUppercaseLetter = prompt("Would you like to include uppercase letters? (yes or no)").toLowerCase();
+  if (includeUppercaseLetter === "yes") {
+    includeUppercaseLetter = true;
+  } else {
+    includeUppercaseLetter = false;
+  }
+
+  let includeNumbers = prompt("Would you like to include numbers? (yes or no)").toLowerCase();
+  if (includeNumbers === "yes") {
+    includeNumbers = true;
+  } else {
+    includeNumbers = false;
+  }
+
+  let includeSpecialChar = prompt("Would you like to include special characters? (yes or no)").toLowerCase();
+  if (includeSpecialChar === "yes") {
+    includeSpecialChar = true;
+  } else {
+    includeSpecialChar = false;
+  }
+
+  let availableCharacters = '';
+  if (includeLowercaseLetters) {
+    availableCharacters += lowercaseLetters;
+  }
+  if (includeUppercaseLetter) {
+    availableCharacters += uppercaseLetter;
+  }
+  if (includeNumbers) {
+    availableCharacters += numbers;
+  }
+  if (includeSpecialChar) {
+    availableCharacters += specialChar;
+  }
+
+  function generatePassword(length, availableCharacters) {
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      password += availableCharacters.charAt(Math.floor(Math.random() * availableCharacters.length));
+    }
+    return password;
+  }
+
+  let password = generatePassword(passLength, availableCharacters);
+  let passwordText = document.querySelector("#password");
   passwordText.value = password;
-
+  return password;
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
